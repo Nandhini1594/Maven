f8e58b312d1cbbe5f2158919163d91fe61ad9ed8
Nandhini1594 <nandhini.civil94@gmail.com>
2023-01-06 11:40:13 +0530

    First login message

---
+++ src/test/java/com/test/Greens.java
@@ -0,0 +1,17 @@
+package com.test;
+
+public class Greens {
+	private void greens1() {
+		System.out.println("Chennai Branch");
+	}
+
+	private void greens2 () {
+		System.out.println("Trichy Branch");
+	}
+
+	private void greens3() {
+		System.out.println("Salem Branch");
+
+	}
+
+}
